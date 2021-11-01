59fd408c8dff7aad6bd27372e538e118a8a118e5
Vitaliy Tikhanov <love-vitaliy@ya.ru>
2021-11-01 23:37:36 +0300

    vue js vorks

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
 require('./bootstrap');
+
+import Vue from "vue"
+import App from "./components/App";
+Vue.component('app',App)
+
+const app = new Vue({
+    el: '#app',
+
+});
---
+++ resources/js/components/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+            <h1>Hi!</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "App"
+}
+</script>
+
+<style scoped>
+
+</style>
